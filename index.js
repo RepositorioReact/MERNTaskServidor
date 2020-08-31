6881cf6b08d8633d2f2a618c7b8bfe23c234313a
Paco <paco@MacBook-Air-de-Paco.local>
2020-08-31 20:38:43 +0200

    cambio de puerto

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({extended:true}));
 
 //puerto de la app
-const port = process.env.port || config.httpPort;
+const PORT = process.env.port || config.httpPort;
 
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -25,6 +25,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 //arrancar la app //0.0.0.0 para que heroku lo ponga por defecto
-app.listen(port,() => {
-    console.log(`El servidor está funcionando en el puerto ${port}`);
+app.listen(PORT,() => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
 })
